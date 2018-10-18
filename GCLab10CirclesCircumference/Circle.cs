c7f42f054dcabd78ff0eec2854a0c4c04158cb4c
Sean Keckler <seankeckler@gmail.com>
2018-10-18 07:04:10 -0400

    Initial Commit

---
+++ GCLab10CirclesCircumference/Circle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GCLab10CirclesCircumference
+{
+    class Circle
+    {
+        private double Radius; 
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+            
+        }
+
+        public double CalculateCircumference()
+        {
+            double pi = Math.PI;
+            double circumference = 2 * pi * Radius;
+            return circumference;
+            
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            double circumference = CalculateCircumference();
+            string formatedCircumference = FormatNumber(circumference);
+            return formatedCircumference;
+                
+            
+        }
+
+        public double CalculateArea()
+        {
+            double pi = Math.PI;
+            double area = pi * Radius * Radius;
+            return area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            double Calculatearea = CalculateArea();
+            string formatedArea = FormatNumber(Calculatearea);
+            return formatedArea;
+        }
+
+        private string FormatNumber(double x)
+        {
+
+            x = Math.Truncate( x * 100) / 100;
+            return x.ToString();
+        }
+
+
+
+    }
+}
---
+++ GCLab10CirclesCircumference/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCLab10CirclesCircumference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Circle> circles = new List<Circle>();
+            bool run = true;
+            while (run == true) 
+            {
+                Console.WriteLine("Welcome to the Circle Tester");
+                Console.Write("Enter your circle's radius: ");
+                double radius;
+                double.TryParse(Console.ReadLine(), out radius);
+                if (radius > 0)
+                {
+                    Circle circle = new Circle(radius);
+                    circles.Add(circle);
+                    Console.WriteLine("The Circumference of your circle is: " + circle.CalculateFormattedCircumference());
+                    Console.WriteLine("The Area of your circle is: " + circle.CalculateFormattedArea());
+                    run = Continue();
+                    if (run == false)
+                    {
+                        Console.WriteLine("Goodbye, you made {0} circles", circles.Count());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid input.");
+                }
+                
+            }
+            Console.ReadKey();
+
+        }
+        public static bool Continue()
+        {
+            bool run;
+            Console.WriteLine("Continue? y/n");
+            string answer = Console.ReadLine();
+            answer = answer.ToLower();
+            if (answer == "y")
+            {
+                run = true;
+            }
+            else if (answer == "n")
+            {
+                run = false;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, I didn't understand that. Try again.");
+                run = Continue();
+            }
+            return run;
+        }
+
+    }
+}
+
